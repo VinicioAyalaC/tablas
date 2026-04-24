7e054006f93fba3deff97950f69fc4cc080a0c44
VinicioAyalaC <vhac2311@gmail.com>
2026-04-23 21:11:33 -0500

    genera tabla del 3

--- multiplicar.js
+++ multiplicar.js
@@ -1,4 +1,11 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedorTabla");
     contenedor.innerHTML = "<h1>PROBANDO</h1>";
+
+    let contenido="";
+    
+    for(let i=1; i<=10; i++){
+        contenido= contenido + "<td>"+' 3 * '+i+" </td> " + "<td> "+(3*i)+" </td>"; 
+    }
+    contenedor.innerHTML = contenido;
 }
